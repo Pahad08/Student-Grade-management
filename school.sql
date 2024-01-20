1b8b59c32e9c7c2bef8187d8a2f51735313d8823
Pahad08 <mastahpahad@gmail.com>
2024-01-20 23:23:07 +0800

    changed the database

--- school.sql
+++ school.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2023 at 03:56 PM
+-- Generation Time: Jan 20, 2024 at 04:22 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -39,8 +39,12 @@ CREATE TABLE `accounts` (
 --
 
 INSERT INTO `accounts` (`account_id`, `username`, `email`, `password`) VALUES
-(1, 'admin', 'mastahpahad@gmail.com', '$2y$10$vtPVSS0KuRwV85cuxzH2TuL22WbXDWpRyDkiGdQgpaXYCWr133Axa'),
-(2, 'fahad', 'fmbagundang.stjohn@gmail.com', '$2y$10$8cQsmcIv/ZQOlmwrhc9yiOYrnqAUIUuwIO.Tp7HdRr/tcwyUwCq16');
+(1, 'admin', 'mastahpahad@gmail.com', '$2y$10$lyEbsnQjE2mYvryS19flheCeLanG1ZaAuZw6crJpsNki0eNhpTp/C'),
+(32, 'Cookiee', 'cookie@email.com', '$2y$10$xfOvMeuylL.9LZDak29sqOKnY.GQDYsEPWmm4m4iU6xD/AdxguATy'),
+(36, 'Moine', 'moine@gmail.com', '$2y$10$xZylN1L6DkeKNsFmqpw4/uRu/y6eTXu2Ro.MlCAAnCxCyoMb8NIyi'),
+(37, 'crumy', 'crumble@gmail.com', '$2y$10$/AhaM1JlxHTaU1nj801o9uYWyEpnXRKqrobaAxDYTI5EhEHckK/cC'),
+(68, 'sweet', 'sweetlamanero21@gmail.com', '$2y$10$JvHLw9yms34XAE.QXMgeMutIEnYQAkfBZti3W.Zh/yBzWLwLOeu4m'),
+(70, 'mugi', 'mugi@email.com', '$2y$10$.wruF6KYnX3CZj02tIFLUuRcwoi/9MKdR6hBUBi.XzY0.pMfw/QLG');
 
 -- --------------------------------------------------------
 
@@ -71,8 +75,19 @@ INSERT INTO `admins` (`admin_id`, `account_id`, `f_name`, `l_name`) VALUES
 CREATE TABLE `grades` (
   `grade_id` int(11) NOT NULL,
   `grade` int(2) NOT NULL,
-  `student_id` int(11) NOT NULL
+  `student_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `grades`
+--
+
+INSERT INTO `grades` (`grade_id`, `grade`, `student_id`, `subject_id`) VALUES
+(17, 89, 21, 69),
+(18, 85, 38, 71),
+(22, 85, 24, 73),
+(24, 95, 23, 72);
 
 -- --------------------------------------------------------
 
@@ -84,6 +99,7 @@ CREATE TABLE `students` (
   `student_id` int(11) NOT NULL,
   `f_name` varchar(255) NOT NULL,
   `l_name` varchar(255) NOT NULL,
+  `gender` varchar(1) NOT NULL,
   `contact_number` varchar(11) NOT NULL,
   `section` varchar(1) NOT NULL,
   `grade_level` varchar(20) NOT NULL,
@@ -95,8 +111,11 @@ CREATE TABLE `students` (
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`student_id`, `f_name`, `l_name`, `contact_number`, `section`, `grade_level`, `profile_pic`, `account_id`) VALUES
-(3, 'Fahad', 'Bagundang', '09365896124', 'A', '3rd-year', '', 2);
+INSERT INTO `students` (`student_id`, `f_name`, `l_name`, `gender`, `contact_number`, `section`, `grade_level`, `profile_pic`, `account_id`) VALUES
+(21, 'Cookie', 'Lamanero', 'M', '09365896124', 'A', 'Grade 7', '..\\profile_pics\\20220307_174119.jpg', 32),
+(23, 'Moine', 'Lamanero', 'F', '09365896124', 'A', 'Grade 9', '..\\profile_pics\\20220225_193728.jpg', 36),
+(24, 'Crumble', 'Lamanero', 'M', '09365896124', 'A', 'Grade 8', '..\\profile_pics\\20220225_204003.jpg', 37),
+(38, 'Mugi', 'Lamanero', 'M', '09365896124', 'A', 'Grade 10', '..\\profile_pics\\user.png', 70);
 
 -- --------------------------------------------------------
 
@@ -110,6 +129,38 @@ CREATE TABLE `subjects` (
   `subject` varchar(50) NOT NULL,
   `description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`subject_id`, `code`, `subject`, `description`) VALUES
+(69, 'TLE', 'Technology and Livelihood Education', 'One of the learning areas of the Secondary Education Curriculum used in Philippine secondary schools.'),
+(71, 'AP', 'Araling Panlipunan', 'Sample'),
+(72, 'ENG', 'English', 'English'),
+(73, 'FIL', 'Filipino', 'Filipino');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `teacher_id` int(11) NOT NULL,
+  `f_name` varchar(100) NOT NULL,
+  `l_name` varchar(100) NOT NULL,
+  `gender` varchar(1) NOT NULL,
+  `profile_pic` varchar(100) NOT NULL,
+  `account_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`teacher_id`, `f_name`, `l_name`, `gender`, `profile_pic`, `account_id`) VALUES
+(6, 'Sweet', 'Lamanero', 'F', '..\\profile_pics\\IMG_20210812_151856_135.jpg', 68);
 
 -- --------------------------------------------------------
 
@@ -147,14 +198,14 @@ ALTER TABLE `admins`
 --
 ALTER TABLE `grades`
   ADD PRIMARY KEY (`grade_id`),
-  ADD KEY `student_id` (`student_id`);
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `subject_id` (`subject_id`);
 
 --
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`student_id`),
-  ADD UNIQUE KEY `contact_number` (`contact_number`),
   ADD KEY `account_id` (`account_id`);
 
 --
@@ -163,6 +214,13 @@ ALTER TABLE `students`
 ALTER TABLE `subjects`
   ADD PRIMARY KEY (`subject_id`),
   ADD UNIQUE KEY `subject` (`subject`);
+
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`teacher_id`),
+  ADD KEY `account_id` (`account_id`);
 
 --
 -- Indexes for table `token`
@@ -178,7 +236,7 @@ ALTER TABLE `token`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT for table `admins`
@@ -190,25 +248,31 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `grades`
 --
 ALTER TABLE `grades`
-  MODIFY `grade_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `grade_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `token`
 --
 ALTER TABLE `token`
-  MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- Constraints for dumped tables
@@ -224,13 +288,20 @@ ALTER TABLE `admins`
 -- Constraints for table `grades`
 --
 ALTER TABLE `grades`
-  ADD CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `grades_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`subject_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `students`
 --
 ALTER TABLE `students`
   ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD CONSTRAINT `teachers_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
